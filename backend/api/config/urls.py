595d8b9693ca7216b66b3c2bb55babf7888bf6e9
khirooka <khirooka>
2023-06-18 15:42:38 +0900

    urls修正

--- backend/api/config/urls.py
+++ backend/api/config/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/auth/', include('djoser.urls')),
-    path('api/auth/', include('djoser.urls.jwt')),
-    path('api/', include('accounts.urls')),
     path('admin/', admin.site.urls),
+    path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/auth/', include('djoser.urls.jwt')),
+    path('api/v1/', include('accounts.urls')),
 ]
