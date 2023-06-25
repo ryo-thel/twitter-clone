d29db98fef2629e9db3b8efd5e68d50d67545663
ryo-thel <uyori0303@gmail.com>
2023-06-25 19:19:05 +0900

    add : activate view

--- backend/api/accounts/urls.py
+++ backend/api/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserList
+from .views import UserList, Activate
 
 urlpatterns = [
     path('users/', UserList.as_view()),
+    path('activate/<str:uid>/<str:token>/', Activate),
 ]
