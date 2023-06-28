abd4c2c4b70e5fcbd8ea047fe587bd96b6b98073
ryo-thel <uyori0303@gmail.com>
2023-06-28 19:40:57 +0900

    fix : add return

--- app/src/routes/Activation.js
+++ app/src/routes/Activation.js
@@ -4,17 +4,33 @@ import { useParams } from "react-router-dom";
 
 export default function Activation() {
     const { uid, token } = useParams();
+    const [Success, setSuccess] = useState(false);
 
     useEffect(() => {
         if (uid && token) {
             authApi.Activate(uid, token)
                 .then(data => {
                     console.log('成功しました', data);
+                    setSuccess(true);
                 })
                 .catch(error => {
                     console.log('失敗しました', error);
                 });
         }
     }, [uid, token]);
+
+    if (Success) {
+        return (
+            <div>
+                <h1>本登録完了</h1>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <h1>もう一度やり直してください</h1>
+            </div>
+        )
+    }
 
 }
