891142f5fc1775848a0dde040eb5746756238843
ryo <r.hira.22.9.629@4nonome.com>
2023-06-28 16:51:33 +0900

    fix : remove some waste import

--- backend/api/accounts/views.py
+++ backend/api/accounts/views.py
@@ -1,12 +1,6 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.tokens import default_token_generator
-from rest_framework import status
 from rest_framework.generics import ListAPIView
-from rest_framework.response import Response
 from .serializers import CustomUserSerializer
-from djoser import utils
-from djoser.compat import get_user_email
-from djoser.conf import settings
 
 User = get_user_model()
 
