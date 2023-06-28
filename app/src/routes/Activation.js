c80ee8a621c9a27a8789992aa5c28f73e933e096
ryo-thel <uyori0303@gmail.com>
2023-06-28 19:42:53 +0900

    fix : add import useState

--- app/src/routes/Activation.js
+++ app/src/routes/Activation.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import authApi from '../api/authApi';
 import { useParams } from "react-router-dom";
 
