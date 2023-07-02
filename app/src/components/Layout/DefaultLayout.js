e05e7fc78adba6ac0291609bab5d3f519a369693
khirooka <khirooka>
2023-07-02 18:02:31 +0900

    "fix: DefaultLayout"

--- app/src/components/Layout/DefaultLayout.js
+++ app/src/components/Layout/DefaultLayout.js
@@ -3,7 +3,7 @@ import Header from "../Header";
 
 const LoginButtonObject = [{ title: "LOG IN", url: "/login" }];
 
-export default DefaultLayout = ({ children }) => {
+export default function DefaultLayout({ children }){
   return (
     <>
       <Header title="TwtterClone-C" sections={LoginButtonObject} />
