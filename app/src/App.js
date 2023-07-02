b987ea60ccdc44753cea2a549fbb94e66f1aa1c3
khirooka <khirooka>
2023-07-02 17:28:34 +0900

    fix: import

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,9 @@
 import React from "react";
 import SignUp from "./pages/SignUp";
-import Header from "./components/Header";
-// import LoggedIn from "./components/LoggedIn";
-// import LoggedOut from "./components/LoggedOut";
 import { Route, Routes } from "react-router-dom";
 import { CookiesProvider, withCookies } from "react-cookie";
 import DefaultLayout from "./components/Layout/DefaultLayout";
-// import LoggedIn from "./components/LoggedIn";
-import Activation from './routes/Activation';
+import Activation from './pages/Activation';
 
 function App() {
   return (
