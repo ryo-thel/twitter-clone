1dae88b961d7ceecc83023ce436da9d06b7cf07f
ryo-thel <uyori0303@gmail.com>
2023-07-06 22:06:28 +0900

    cookieのJWT関連の設定

--- backend/api/accounts/authentication.py
+++ backend/api/accounts/authentication.py
@@ -1,5 +1,5 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
 from django.conf import settings
 
 # tokenをauthorizationヘッダーからではなくcookieヘッダーから取得
@@ -8,11 +8,9 @@ class CookieJWTAuthentication(JWTAuthentication):
         # Cookieヘッダーからaccess_tokenを取得
         access_token = request.COOKIES.get('access_token')
         if not access_token:
-            Response({"message": 'no Token'})
-        else:
-            Response(access_token)
+            raise AuthenticationFailed('No access token')
         # access_tokenが存在すればauthrizationヘッダーに追加
-        if access_token:
+        else :
             request.META['HTTP_AUTHORIZATION'] = '{header_type} {access_token}'.format(
                 header_type=settings.SIMPLE_JWT['AUTH_HEADER_TYPES'][0], access_token=access_token)
         # 親クラスに返す
--- backend/api/accounts/urls.py
+++ backend/api/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserList, JWTokenObtainView, JWTokenRefreshView
+from .views import UserList, JWTokenObtainView, JWTokenRefreshView, LogoutView
 
 urlpatterns = [
     path('users/', UserList.as_view()),
     path('auth/jwt/create/', JWTokenObtainView.as_view()),
-    path('auth/jwt/refresh', JWTokenRefreshView.as_view(),)
+    path('auth/jwt/refresh/', JWTokenRefreshView.as_view()),
+    path('auth/logout/', LogoutView.as_view()),
 ]
