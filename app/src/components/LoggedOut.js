e668e52a49219bf3f581ffd32d61a7a3e3a59574
khirooka <khirooka>
2023-07-09 20:18:00 +0900

    fix

--- app/src/components/LoggedOut.js
+++
@@ -1,6 +0,0 @@
-// import { withCookies } from "react-cookie";
-
-// const LoggedOut = (props) =>
-//   props.cookies.get("token") ? null : props.children;
-
-// export default withCookies(LoggedOut);
