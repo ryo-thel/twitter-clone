52a6493ef37b0058eea2e7a1d648b5128f70bcfd
khirooka <khirooka>
2023-07-10 20:14:29 +0900

    fix : EmailFormコンポーネント作成

--- app/src/pages/Activation.js
+++ app/src/pages/Activation.js
@@ -73,85 +73,23 @@ const defaultTheme = createTheme();
 export function ResendActivation() {
     const navigate = useNavigate();
     const [errorMessage, setError] = useState("");
+
     const handleSubmit = (event) => {
-    event.preventDefault();
-
-
-    const data = new FormData(event.currentTarget);
-
-    authApi.ResendActivation(data)
-        .then((res) => {
-            console.log('成功しました', res);
-            navigate("/");
-            setError("");
-        })
-        .catch((error) => {
-            console.log(error)
-            setError(error.response.data);
-        });
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        authApi.ResendActivation(data)
+            .then((res) => {
+                console.log('成功しました', res);
+                navigate("/");
+                setError("");
+            })
+            .catch((error) => {
+                console.log(error)
+                setError(error.response.data);
+            });
     };
 
     return(
-        <ThemeProvider theme={defaultTheme}>
-        <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-            sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            }}
-        >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-            Resend Activation
-        </Typography>
-        <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 3 }}
-        >
-        {errorMessage.non_field_errors ? (
-            <p className="red">{errorMessage.non_field_errors}</p>
-            ) : null}
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                <TextField
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                />
-                </Grid>
-            {errorMessage.email ? (
-                <p className="red">{errorMessage.email}</p>
-                ) : null}
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Send
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-    </ThemeProvider>
+        <EmailForm handleSubmit={handleSubmit} errorMessage={errorMessage} />
     )
 }
