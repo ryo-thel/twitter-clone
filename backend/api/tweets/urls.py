07e5dfe019b178948e4b2511e72a2586a66b5668
shiota <t.shiota@4nonome.com>
2023-07-16 05:56:39 +0900

    tweet/views.py DeleteviewとDetailViewの実装1

--- backend/api/tweets/urls.py
+++ backend/api/tweets/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import TweetListCreateView
+from .views import TweetListCreateView, TweetDeleteView, TweetDetailView
 
 urlpatterns = [
   path('', TweetListCreateView.as_view()),
+  path('', TweetDeleteView.as_view()),
+  path('', TweetDetailView.as_view()),
 ]
--- backend/api/tweets/views.py
+++ backend/api/tweets/views.py
@@ -1,7 +1,31 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, GenericAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.mixins import RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin
 from .models import Tweet
+from .serializers import TweetSerializer
+
 
 class TweetListCreateView(ListCreateAPIView):
     queryset = Tweet.objects.all()
     permission_classes = (IsAuthenticated,)
+
+
+class TweetDeleteView(RetrieveModelMixin, DestroyModelMixin, GenericAPIView):
+    queryset = Tweet.objects.all()
+    serializer_class = TweetSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+class TweetDetailView(RetrieveModelMixin, UpdateModelMixin, GenericAPIView):
+    queryset = Tweet.objects.all()
+    serializer_class = TweetSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
