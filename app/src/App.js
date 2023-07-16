93fd3b0427fa6da9add5c6f47610bc30cb8ed6e6
i-mak33-124 <i.124.takumi00@gmail.com>
2023-07-16 15:59:19 +0900

    reset_username,reset_email

--- app/src/App.js
+++ app/src/App.js
@@ -4,6 +4,8 @@ import { Route, Routes } from "react-router-dom";
 import { CookiesProvider, withCookies } from "react-cookie";
 import DefaultLayout from "./components/Layout/DefaultLayout";
 import { Activation, ResendActivation } from './pages/Activation';
+import { ResetPassword, ResetUsername} from './pages/Confirm';
+import ResetEmailForm from './pages/Reset';
 import Login from "./pages/Login";
 import Logout from "./pages/Logout";
 import SetUsername from "./pages/SetUsername";
@@ -24,6 +26,10 @@ function App() {
             <Route path="/setpassword" element={<SetPassword/>} />
             <Route path="/user" element={<User />} />
             <Route path="/resendactivation" element={<ResendActivation />} />
+            <Route path="/reset_password_email_form" element={<ResetEmailForm reset_of="password" />} />
+            <Route path="/reset_password_confirm/:uid/:token" element={<ResetPassword />} />
+            <Route path="/reset_username_email_form" element={<ResetEmailForm reset_of="username"/>} />
+            <Route path="/reset_username_confirm/:uid/:token" element={<ResetUsername />} />
             <Route path="/" />
           </Route>
         </Routes>
