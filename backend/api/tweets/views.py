b26299bce19d72f20b3b3c8982fc8c7c5d8be9d9
khirooka <l.00.kocha.00.l@gmail.com>
2023-08-09 19:07:00 +0900

    fix: openAPI更新

--- backend/api/tweets/views.py
+++ backend/api/tweets/views.py
@@ -2,6 +2,7 @@ from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from .models import Tweet
 
+
 class TweetListCreateView(ListCreateAPIView):
     queryset = Tweet.objects.all()
     permission_classes = (IsAuthenticated,)
