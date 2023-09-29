1ea33cccf9831f9e8c55925964148d9e6cf9e6f7
ryo-thel <uyori0303@gmail.com>
2023-09-29 19:37:12 +0900

    toastcontainerを削除

--- app/src/pages/Logout.js
+++ app/src/pages/Logout.js
@@ -1,39 +1,42 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import authApi from '../api/authApi';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Logout = () => {
-  const [Success, setSuccess] = useState(false);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    authApi.Logout()
-      .then(data => {
-        console.log('成功しました', data);
-        setSuccess(true);
-        // Display success toast
-        toast.success('ログアウト完了', {
-          onClose: () => navigate("/login"),  // Redirect to login on toast close
-          autoClose: 2000,  // Set autoClose time
+  const handleLogout = () => {
+    // Display confirmation dialog
+    const confirmation = window.confirm("Logoutしますか?");
+    if (confirmation) {
+      // Proceed with logout if user confirms
+      authApi.Logout()
+        .then(data => {
+          console.log('成功しました', data);
+          // Display success toast
+          toast.success('ログアウト完了', {
+            onClose: () => navigate("/login"),  // Redirect to login on toast close
+            autoClose: 2000,  // Set autoClose time
+          });
+        })
+        .catch(error => {
+          console.log('失敗しました', error);
+          // Display error toast
+          toast.error('ログアウト失敗', {
+            onClose: () => navigate("/"),
+            autoClose: 2000,  // Set autoClose time
+          });
         });
-      })
-      .catch(error => {
-        console.log('失敗しました', error);
-      });
-  }, [navigate]);
+    }
+  };
 
   return (
     <div>
-      <ToastContainer />  {/* ToastContainer to display toast */}
-      {Success ? (
-        <h1>ログアウト完了</h1>
-      ) : (
-        <h1>もう一度やり直してください</h1>
-      )}
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
-}
+};
 
 export default Logout;
